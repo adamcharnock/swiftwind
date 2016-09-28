5096a235d7cbd1ad9be0ce9f4d0fd8c4c48c17f3
Adam Charnock <acharnock@twitter.com>
2016-09-28 02:05:55 +0100

    Adding tests for recent changes

--- swiftwind/transactions/tests.py
+++ swiftwind/transactions/tests.py
@@ -605,21 +605,21 @@ class ReconcileTransactionsViewTestCase(TestCase):
             date='2000-01-01',
             statement_import=statement_import,
             amount=Decimal('100.16'),
-            description='Item description',
+            description='Item description 1',
         )
 
         self.line2 = StatementLine.objects.create(
             date='2000-01-03',
             statement_import=statement_import,
             amount=Decimal('-50.12'),
-            description='Item description',
+            description='Item description 2',
         )
 
         self.line3 = StatementLine.objects.create(
             date='2000-01-06',
             statement_import=statement_import,
             amount=Decimal('40.35'),
-            description='Item description',
+            description='Item description 3',
         )
 
     def test_get(self):
@@ -637,6 +637,12 @@ class ReconcileTransactionsViewTestCase(TestCase):
         self.assertEqual(response.context['statement_lines'].count(), 3)
         self.assertTrue(response.context['transaction_form'])
         self.assertTrue(response.context['leg_formset'])
+
+        transaction_form = response.context['transaction_form']
+        self.assertEqual(transaction_form.initial['description'], 'Item description 1')
+
+        leg_formset = response.context['leg_formset']
+        self.assertEqual(leg_formset.forms[0].initial['amount'], Decimal('100.16'))
 
     def test_post_reconcile_valid_one(self):
         response = self.client.post(self.view_url, data={
@@ -720,3 +726,58 @@ class ReconcileTransactionsViewTestCase(TestCase):
         leg_formset = response.context['leg_formset']
         self.assertEqual(leg_formset.non_form_errors(), ['Amounts must add up to 100.16'])
         self.assertIn('Amounts must add up to 100.16', response.content.decode('utf8'))
+
+    def test_post_reconcile_negative_amount(self):
+        response = self.client.post(self.view_url, data={
+            'reconcile': self.line1.uuid,
+            'description': 'Test transaction',
+
+            'legs-INITIAL_FORMS': '0',
+            'legs-TOTAL_FORMS': '2',
+
+            'legs-0-amount': '-1',
+            'legs-0-account': self.income_account.uuid,
+            'legs-0-id': '',
+        })
+        self.assertEqual(Transaction.objects.count(), 0)
+
+        leg_formset = response.context['leg_formset']
+        self.assertEqual(leg_formset.errors[0]['amount'], ['Amount must be greater than zero'])
+
+    def test_post_reconcile_zero_amount(self):
+        response = self.client.post(self.view_url, data={
+            'reconcile': self.line1.uuid,
+            'description': 'Test transaction',
+
+            'legs-INITIAL_FORMS': '0',
+            'legs-TOTAL_FORMS': '2',
+
+            'legs-0-amount': '0',
+            'legs-0-account': self.income_account.uuid,
+            'legs-0-id': '',
+        })
+        self.assertEqual(Transaction.objects.count(), 0)
+
+        leg_formset = response.context['leg_formset']
+        self.assertEqual(leg_formset.errors[0]['amount'], ['Amount must be greater than zero'])
+
+    def test_post_reconcile_negative(self):
+        """Check that that positive amounts will be correctly used to reconcile negative amounts"""
+        response = self.client.post(self.view_url, data={
+            'reconcile': self.line2.uuid,
+            'description': 'Test transaction',
+
+            'legs-INITIAL_FORMS': '0',
+            'legs-TOTAL_FORMS': '2',
+
+            'legs-0-amount': '50.12',
+            'legs-0-account': self.income_account.uuid,
+            'legs-0-id': '',
+        })
+
+        if 'transaction_form' in response.context:
+            self.assertFalse(response.context['transaction_form'].errors)
+            self.assertFalse(response.context['leg_formset'].errors)
+
+        transaction = Transaction.objects.get()
+        self.assertEqual(transaction.legs.count(), 2)
