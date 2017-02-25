37b330bbd4a48784e47a3ba0a87f1eb26e4be875
Adam Charnock <adam@adamcharnock.com>
2017-02-25 19:41:50 +0000

    Bumping hordak version requirement

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'django>=1.8',
-        'django-hordak>=1.1.0',
+        'django-hordak>=1.3.0',
         'path.py',
         'django-model-utils>=2.5.0',
         'gunicorn',
