ac7760e2984d193ea86d5fe317144de8ec1648e4
Adam Charnock <adam@adamcharnock.com>
2017-02-25 19:40:09 +0000

    Fixes for changes in hordak

--- swiftwind/transactions/urls.py
+++ swiftwind/transactions/urls.py
@@ -5,8 +5,8 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', hordak.views.transactions.CreateTransactionView.as_view(), name='create'),
-    url(r'^reconcile/$', hordak.views.transactions.ReconcileTransactionsView.as_view(), name='reconcile'),
+    url(r'^$', hordak.views.transactions.TransactionCreateView.as_view(), name='create'),
+    url(r'^reconcile/$', hordak.views.transactions.TransactionsReconcileView.as_view(), name='reconcile'),
     url(r'^import/$', views.CreateImportView.as_view(), name='import_create'),
     url(r'^import/(?P<uuid>.*)/setup/$', views.SetupImportView.as_view(), name='import_setup'),
     url(r'^import/(?P<uuid>.*)/dry-run/$', views.DryRunImportView.as_view(), name='import_dry_run'),
