44dca55ac8e6ab152e46dc434c480befe0ebc166
Adam Charnock <adam@adamcharnock.com>
2017-02-27 13:28:31 +0000

    Setting allowed hosts to *

--- example_project/settings.py
+++ example_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '7lz1x9*1dfc_6jd-24fszneiude(t%84re6*!)kpc=ifk(7@3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INTERNAL_IPS = ['127.0.0.1']
 
