bb0ad590660b2f8c17bc50079d7cc0e37fb7c4d1
Adam Charnock <adam@adamcharnock.com>
2017-03-04 16:09:35 +0000

    Working on zero value rendering

--- swiftwind/dashboard/tests.py
+++ swiftwind/dashboard/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from swiftwind.core.management.commands.swiftwind_create_accounts import Command
+from swiftwind.utilities.testing import DataProvider
+
+
+class DashboardViewTestCase(DataProvider, TestCase):
+
+    def setUp(self):
+        self.url = reverse('dashboard:dashboard')
+        Command().handle(currency='GBP')
+
+    def test_get(self):
+        self.login()
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
