d11bf111f870d6d20eb9b0c924ed7fe481fe373b
Adam Charnock <adam@adamcharnock.com>
2018-02-09 13:19:21 +0000

    Can now unenact billing cycles

--- swiftwind/billing_cycle/urls.py
+++ swiftwind/billing_cycle/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.BillingCycleListView.as_view(), name='list'),
     url(r'^enact/(?P<uuid>.+)/$', views.CreateTransactionsView.as_view(), name='enact'),
     url(r'^reenact/(?P<uuid>.+)/$', views.RecreateTransactionsView.as_view(), name='reenact'),
+    url(r'^unenact/(?P<uuid>.+)/$', views.DeleteTransactionsView.as_view(), name='unenact'),
     url(r'^send/(?P<uuid>.+)/$', views.SendNotificationsView.as_view(), name='send'),
 ]
